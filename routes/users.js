cd11d6f937e04cffb5de0c3b8d48d6dae6842733
junyeongyu <Junyeong.Yu@MyGeorgian.ca>
2017-03-08 14:50:52 -0500

    add user list

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+"use strict";
+let express = require('express');
+let router = express.Router();
+
+// link to your existing Account model
+let Account = require('../models/account');
+
+// auth check
+function isLoggedIn(req, res, next) {
+   if (req.isAuthenticated()) {
+      return next(); // user is logged, so call the next function
+   }
+   res.redirect('/'); // not logged in so redirect to home
+}
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', isLoggedIn, function(req, res, next) {
+  Account.find(function(err, users) {
+    if (err) {
+      console.log(err);
+      res.end(err);
+      return;
+    }
+    
+    // no error so send the books to the index view
+    res.render('users/index', {
+      users: users,
+      title: 'User List',
+      user: req.user
+    });
+  });
 });
 
 module.exports = router;
